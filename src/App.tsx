5fc930d6ce8e3712934cb2b4c268f4975fcd55d1
bea <beatrice.corti@proximitybbdo.it>
2025-11-15 13:29:17 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="min-h-screen bg-secondary font-sans">
+      <p className='text-5xl font-medium text-primary'>Who Sings?</p>
+    </div>
+    </>
+  )
+}
+
+export default App
